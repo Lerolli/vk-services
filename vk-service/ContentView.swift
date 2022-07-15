3719a9563fdacc0d28d252301734957e917258f8
Anton Zakharov <zaharov.28097411@gmail.com>
2022-07-15 09:12:17 +0500

    Initial Commit

---
+++ vk-service/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  vk-service
+//
+//  Created by Антон Захаров on 15.07.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ vk-service/vk_serviceApp.swift
@@ -0,0 +1,17 @@
+//
+//  vk_serviceApp.swift
+//  vk-service
+//
+//  Created by Антон Захаров on 15.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct vk_serviceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
